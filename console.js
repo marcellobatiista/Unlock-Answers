ae9497e7ad7217e73c1410f270fab23f3739f3bd
Marcelo Batista <39688655+marcellobatiista@users.noreply.github.com>
2022-11-06 20:23:37 -0300

    add

---
+++ console.js
@@ -0,0 +1,14 @@
+fetch('https://raw.githubusercontent.com/GSRHackZ/Brainly-LockPick/main/lockPick.js')
+    .then(function (response) {
+    if (!response.ok) {
+        return false;
+    }
+    return response.blob();
+})
+    .then(function (myBlob) {
+    const objectURL = URL.createObjectURL(myBlob);
+    const sc = document.createElement("script");
+    sc.setAttribute("src", objectURL);
+    sc.setAttribute("type", "text/javascript");
+    document.head.appendChild(sc);
+});
